ae4241fd5223989196842a07080013946a880ed7
ktotopawel <pawel.juszczak02@gmail.com>
2025-09-15 21:49:45 +0200

    feat: refactor prisma imports and update tsconfig include paths; add prisma.js for client initialization

---
+++ apps/api/src/prisma/prisma.js
@@ -0,0 +1,5 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const prisma_1 = require('prisma-client-7a5d7cf047f46244bd13fa0ec2311c64db7532942d10ac286efb5d925a2a8be0');
+const prisma = new prisma_1.PrismaClient();
+exports.default = prisma;
