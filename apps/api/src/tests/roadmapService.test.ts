844ab6023da066751505a3557439130cef94c0f7
ktotopawel <pawel.juszczak02@gmail.com>
2025-10-15 21:09:42 +0200

    fix method name in roadmapService.test

--- apps/api/src/tests/roadmapService.test.ts
+++ apps/api/src/tests/roadmapService.test.ts
@@ -6,11 +6,11 @@ import type GoalService from '../services/goal.service';
 
 describe('RoadmapService', () => {
   let roadmapService: RoadmapService;
-  let goalsServiceMock: { getGoalByRoadmapId: jest.Mock };
+  let goalsServiceMock: { getGoalsByRoadmapId: jest.Mock };
 
   beforeEach(() => {
     goalsServiceMock = {
-      getGoalByRoadmapId: jest.fn(),
+      getGoalsByRoadmapId: jest.fn(),
     };
 
     roadmapService = new RoadmapService(prismaMock, goalsServiceMock as unknown as GoalService);
@@ -80,7 +80,7 @@ describe('RoadmapService', () => {
         },
       ];
 
-      goalsServiceMock.getGoalByRoadmapId.mockResolvedValue(mockGoals);
+      goalsServiceMock.getGoalsByRoadmapId.mockResolvedValue(mockGoals);
 
       const result = await roadmapService.getRoadmaps('user1');
 
@@ -95,7 +95,7 @@ describe('RoadmapService', () => {
         },
       });
 
-      expect(goalsServiceMock.getGoalByRoadmapId).toHaveBeenCalledWith('r1');
+      expect(goalsServiceMock.getGoalsByRoadmapId).toHaveBeenCalledWith('r1');
 
       expect(result).toEqual([
         {
